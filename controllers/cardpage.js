6f661216951f2ede155bae588ff63175be6e10ad
gregali9311 <gjali@asu.edu>
2022-09-29 20:53:44 -0700

    Got handlebars working for all pages

--- controllers/cardpage.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-
-router.get('/cards', async (req, res) => {
-  // Send the rendered Handlebars.js template back as the response
-  res.render('cardpage');
-});
-
-module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -3,12 +3,12 @@ const router = require('express').Router();
 // const apiRoutes = require('/api');
 const homeRoutes = require('./home-routes.js');
 
-const cardpage = require('./cardpage.js');
+// const cardpage = require('./cardpage.js');
 
 router.use('/', homeRoutes);
 // router.use('/', apiRoutes);
 
-router.use('/cards', cardpage);
+// router.use('/cards', cardpage);
 // router.use('/', apiRoutes);
 
 module.exports = router;
